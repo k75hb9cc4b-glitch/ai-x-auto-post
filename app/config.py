1289f1a277ce07a44e99f1be11b94f3026333400
k75hb9cc4b-glitch <k75hb9cc4b@privaterelay.appleid.com>
2026-07-11 07:29:04 +0000

    インシデント修正

--- app/config.py
+++ app/config.py
@@ -7,5 +7,5 @@ X_API_SECRET = os.getenv("X_API_SECRET")
 X_ACCESS_TOKEN = os.getenv("X_ACCESS_TOKEN")
 X_ACCESS_TOKEN_SECRET = os.getenv("X_ACCESS_TOKEN_SECRET")
 
-GOOGLE_CREDENTIALS = "fanza-auto-poster-502020-5f67a2f3c0ff.json"
+GOOGLE_CREDENTIALS = "service_account.json"
 SHEET_NAME = "FANZA"
